fb5cb4da28c7b057d5594879330384425c6f2e22
OmarSalehGeneva <omar.saleh@crea-inseec.com>
2021-03-17 12:04:36 +0100

    family mod

--- src/Controller/FamilyController.php
+++ src/Controller/FamilyController.php
@@ -8,13 +8,41 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class FamilyController extends AbstractController
 {
+
     /**
-     * @Route("/family", name="family")
+     * @Route("/family", name="family_index")
      */
-    public function index(): Response
+    public function family_index(): Response
     {
-        return $this->render('family/index.html.twig', [
-            'controller_name' => 'FamilyController',
+        return $this->render('family/index.html.twig');
+    }
+
+    /**
+     * @Route("/family/new", name="family_new")
+     */
+    public function family_new(): Response
+    {
+        return $this->render('family/new.html.twig');
+    }
+
+    /**
+     * @Route("/family/{id}", name="family_show")
+     */
+    public function family_show($id): Response
+    {
+        return $this->render('family/show.html.twig', [
+            'id' => $id
         ]);
     }
+
+    /**
+     * @Route("/family/{id}/edit", name="family_edit")
+     */
+    public function family_edit($id): Response
+    {
+        return $this->render('family/edit.html.twig', [
+            'id' => $id
+        ]);
+    }
+
 }
